d9eae334ee92624da33334a7ef24e46b5592446f
dev-sahariar-reza <web.developer.sahariar.reza@gmail.com>
2023-11-21 21:31:07 +0600

    jwt token verify and handle status code

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -2,17 +2,28 @@ import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../provider/AuthProvider";
 import MyToyRow from "../../components/TableRow/MyToyRow";
 import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const MyToys = () => {
   const { user } = useContext(AuthContext);
   const [myToys, setMyToys] = useState([]);
+  const navigate = useNavigate();
 
   const url = `http://localhost:5000/toy?email=${user.email}`;
   useEffect(() => {
-    fetch(url)
+    fetch(url, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("car-access-token")}`,
+      },
+    })
       .then((res) => res.json())
       .then((data) => {
-        setMyToys(data);
+        if (!data.error) {
+          setMyToys(data);
+        } else {
+          navigate("/");
+        }
       });
   }, []);
 
